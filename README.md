e1a5e35167f8c80d54b0a3a3055cef4c55614ba9
Ilayanambi <ilayanambi_ponramu@comcast.com>
2020-05-24 23:53:00 -0400

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # serverless-apigateway-access-logs-plugin
 serverless-apigateway-access-logs-plugin
+
+Plugin to update api gateway access logs.
+
+### Using this pluging
+```
+npm install serverless-apigateway-access-logs-plugin
+```
+
+### serverless.yml
+```
+provider:
+    name: XXX
+    apiGatewayAccessLogs:
+        account: "yyy"
+        logGroup: "xxxxx"
+        format: "{"patchOperations":[{"op":"replace","path":"/*/*/metrics/enabled","value":"false"},{"op":"replace","path":"/*/*/logging/loglevel","value":"OFF"},{"op":"replace","path":"/*/*/logging/dataTrace","value":"false"},{"op":"replace","path":"/tracingEnabled","value":"false"},{"op":"replace","path":"/accessLogSettings/destinationArn","value":"arn:aws:logs:us-east-2:294749062442:log-group:/aws/apigateway/access/dynaoDbtuning"},{"op":"replace","path":"/accessLogSettings/format","value":"{ \"requestId\":\"$context.requestId\", \"ip\": \"$context.identity.sourceIp\", \"caller\":\"$context.identity.caller\", \"user\":\"$context.identity.user\",\"requestTime\":\"$context.requestTime\", \"httpMethod\":\"$context.httpMethod\",\"resourcePath\":\"$context.resourcePath\", \"status\":\"$context.status\",\"protocol\":\"$context.protocol\", \"responseLength\":\"$context.responseLength\" }"}]}"
+
+plugins:
+  - serverless-apigateway-access-logs-plugin
+```
---
+++ index.js
@@ -0,0 +1,85 @@
+'use strict';
+
+const _ = require('underscore');
+
+class ServerlessPlugin {
+    constructor(serverless, options) {
+        this.serverless = serverless;
+        this.options = options;
+        this.provider = serverless ? serverless.getProvider('aws') : null;
+        this.service = serverless.service;
+        this.stage = this.serverless.service.provider.stage;
+        this.region = null;
+
+        if (!this.provider) {
+            throw new Error('This plugin must be used with AWS');
+        }
+
+        this.hooks = {
+            'after:package:packageService': this._addCustomLogging.bind(this),
+            'after:aws:package:finalize:mergeCustomProviderResources': this._addCustomLogging.bind(this)
+        };
+    }
+
+    _addCustomLogging() {
+        var self = this;
+        const template = this.serverless.service.provider.compiledCloudFormationTemplate;
+        var accessLogs = this.serverless.service.provider['apiGatewayAccessLogs'];
+        var stage = this.serverless.variables.options.stage
+        var region = this.serverless.service.provider.region;
+
+        if (this.serverless.variables.options.stage) {
+            stage = this.serverless.variables.options.stage;
+        }
+
+        if (this.serverless.variables.options.region) {
+            region = this.serverless.variables.options.region;
+        }
+
+        Object.keys(template.Resources).forEach(function (key) {
+            var resourceType = template.Resources[key]['Type']
+            if (accessLogs && resourceType === 'AWS::ApiGateway::Deployment') {
+                delete template.Resources[key].Properties.StageName;
+
+                var stageConfig = self._getDefaultStageConfig(region, stage, key, accessLogs)
+
+                if (template.Resources.ApiGatewayStage) {
+                    _.merge(template.Resources.ApiGatewayStage, stageConfig);
+                    self.serverless.cli.log('Merged AWS api gateway custom logs..');
+                } else {
+                    template.Resources.ApiGatewayStage = stageConfig;
+                    self.serverless.cli.log('Updated AWS api gateway custom logs..');
+                }
+            }
+
+            if (template.Resources[key]['Type'] === 'AWS::ApiGateway::ApiKey') {
+                template.Resources[key]['DependsOn'] = 'ApiGatewayStage';
+            }
+        });
+    }
+
+    _getDefaultStageConfig(region, stage, deploymentId, accessLogs) {
+        var stageConfig = {
+            Type: "AWS::ApiGateway::Stage",
+            Properties: {
+                StageName: stage,
+                Description: stage,
+                RestApiId: {
+                    "Ref": "ApiGatewayRestApi"
+                },
+                DeploymentId: (deploymentId) ? {
+                    "Ref": deploymentId
+                } : {
+                    "Ref": "TestDeployment"
+                },
+                AccessLogSetting: {
+                    Format: accessLogs.format,
+                    DestinationArn: 'arn:aws:logs:' + region + ':' + accessLogs.account + ':log-group:' + accessLogs.logGroup
+                }
+            }
+        }
+        return stageConfig
+    }
+}
+
+module.exports = ServerlessPlugin;
